c99c127c8e4b4f5c6530af5838b53dad98df6466
Eren <erencan_inanci@hotmail.com>
2023-02-06 02:31:25 +0300

    p update

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>Color Box Generator</h1>
+        <p>Click boxes to change color</p>
         <ColorArea />
         <button
           className="refresh-button"
           onClick={() => window.location.reload(true)}
         >
-          Refresh page
+          New random colors
         </button>
       </header>
     </div>
